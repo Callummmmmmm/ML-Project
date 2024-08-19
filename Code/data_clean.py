6040507c36ec82ee2e43329a71d3694c2b380918
Callummmmmmm <46402745+Callummmmmmm@users.noreply.github.com>
2024-08-19 17:22:37 +0100

    Time-Based Implement
    Implemented time-based features.

--- Code/data_clean.py
+++ Code/data_clean.py
@@ -7,7 +7,7 @@
 from data_imports import pd
 
 
-# In[6]:
+# In[12]:
 
 
 def data():
@@ -18,4 +18,18 @@ def data():
     df_clean = pd.read_csv(filepath, skiprows = 8, index_col = 0)
 
     return df_clean
+
+def time_based(input_data):
+    df = input_data
+    
+    # Setting index to date-time format.
+    df.index = pd.to_datetime(df.index)
+    
+    # Creating new variables based on date time.
+    df['hour'] = df.index.hour
+    df['day_of_week'] = df.index.dayofweek
+    df['month'] = df.index.month
+    df['time_since_start'] = (df.index - df.index.min()).days
+    
+    return df
 
