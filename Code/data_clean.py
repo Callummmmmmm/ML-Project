38b5e2ec7394c82d1c48d4d8c72fd21f6a49455f
Callummmmmmm <46402745+Callummmmmmm@users.noreply.github.com>
2024-10-09 13:16:09 +0100

    GEOS-FP Data Integration

--- Code/data_clean.py
+++ Code/data_clean.py
@@ -7,7 +7,7 @@
 from data_imports import pd
 
 
-# In[12]:
+# In[2]:
 
 
 def data():
@@ -16,14 +16,14 @@ def data():
     # Reading csv and assigning to a variable.
     # Skipping first 8 rows
     df_clean = pd.read_csv(filepath, skiprows = 8, index_col = 0)
+    
+    # Setting index to date-time format.
+    df_clean.index = pd.to_datetime(df_clean.index)
 
     return df_clean
 
 def time_based(input_data):
     df = input_data
-    
-    # Setting index to date-time format.
-    df.index = pd.to_datetime(df.index)
     
     # Creating new variables based on date time.
     df['hour'] = df.index.hour
@@ -32,4 +32,17 @@ def time_based(input_data):
     df['time_since_start'] = (df.index - df.index.min()).days
     
     return df
+
+def GEOS_FP_A1_data():
+    filepath = '/Users/callumwilson/Documents/GitHub/ML-Project/Input/all_variables.csv'
+    
+    df_clean = pd.read_csv(filepath)
+    
+    df_clean.drop(columns='Unnamed: 0', inplace=True)
+    df_clean.rename(columns={'time': 'Date and Time (UTC)'}, inplace=True)
+    df_clean.set_index('Date and Time (UTC)', inplace=True)
+    df_clean.index = pd.to_datetime(df_clean.index)
+    df_clean.index = df_clean.index + pd.Timedelta(minutes=30)
+
+    return df_clean
 
